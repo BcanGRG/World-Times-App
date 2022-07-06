56f4ea992367ac64f4e9bf17bd4cd89583a25fad
Burak Can <BurakCan_GRG@hotmail.com>
2022-07-07 02:09:34 +0300

    Images ve Icons path leri için bir alan oluşturuldu

---
+++ lib/product/utils/assets_constants.dart
@@ -0,0 +1,7 @@
+class AssetsConstants {
+  //* IMAGES
+  static const String splashScreenLogo = "assets/images/dop_splash_logo.png";
+
+  //* ICONS
+  static const String arrowBackIcon = "assets/icons/ic_arrow_back.png";
+}
