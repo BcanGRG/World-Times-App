ff9be0bc0333c159d7292a8b7e310793c4172473
Burak Can <BurakCan_GRG@hotmail.com>
2022-07-07 02:02:43 +0300

    ScaffoldBackground ve splash color eklendi

--- lib/core/theme/themeDatas.dart
+++ lib/core/theme/themeDatas.dart
@@ -5,14 +5,18 @@ import 'package:world_times_app/product/utils/color_constants.dart';
 class ThemeDatas {
   static final lightTheme = ThemeData(
     appBarTheme: AppBarTheme(backgroundColor: ColorConstants.solitudeBlue),
+    scaffoldBackgroundColor: ColorConstants.zirconWhite,
     primaryColor: ColorConstants.prussianBlue,
+    splashColor: Colors.white,
     backgroundColor: ColorConstants.zirconWhite,
     focusColor: ColorConstants.dullLavenderBlue,
     fontFamily: GoogleFonts.montserrat().fontFamily,
   );
   static final darkTheme = ThemeData(
     appBarTheme: AppBarTheme(backgroundColor: ColorConstants.resolutionBlue),
+    scaffoldBackgroundColor: ColorConstants.prussianBlue,
     primaryColor: Colors.white,
+    splashColor: ColorConstants.resolutionBlue,
     backgroundColor: ColorConstants.prussianBlue,
     focusColor: ColorConstants.resolutionBlue,
     fontFamily: GoogleFonts.montserrat().fontFamily,
