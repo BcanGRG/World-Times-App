65ccbbf56e011ae453039bc36f2347d86d5b62af
Burak Can <BurakCan_GRG@hotmail.com>
2022-07-08 01:26:41 +0300

    Search icon pathi eklendi

--- lib/product/utils/assets_constants.dart
+++ lib/product/utils/assets_constants.dart
@@ -7,4 +7,5 @@ class AssetsConstants {
   static const String arrowRightIcon = "assets/icons/ic_arrow_right.png";
   static const String moonIcon = "assets/icons/ic_moon.png";
   static const String sunIcon = "assets/icons/ic_sun.png";
+  static const String searchIcon = "assets/icons/ic_search.png";
 }
