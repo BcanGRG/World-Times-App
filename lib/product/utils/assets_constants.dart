fece48a8114f3e83ea15fd57008d6ed8c4b4d170
Burak Can <BurakCan_GRG@hotmail.com>
2022-07-07 19:17:43 +0300

    arrow right , moon , sun ikonlarının yolları eklendi

--- lib/product/utils/assets_constants.dart
+++ lib/product/utils/assets_constants.dart
@@ -4,4 +4,7 @@ class AssetsConstants {
 
   //* ICONS
   static const String arrowBackIcon = "assets/icons/ic_arrow_back.png";
+  static const String arrowRightIcon = "assets/icons/ic_arrow_right.png";
+  static const String moonIcon = "assets/icons/ic_moon.png";
+  static const String sunIcon = "assets/icons/ic_sun.png";
 }
